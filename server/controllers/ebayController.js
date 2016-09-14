dfabf7d4c0ed874966f39e2c452045b86606f6b2
Jeff Lam <jefflam1200@gmail.com>
2016-09-14 12:55:00 -0700

    (feature) functional eBay query

--- server/controllers/ebayController.js
+++ server/controllers/ebayController.js
@@ -1,10 +1,9 @@
 (function() {
-
+  var ebayKeys = require('../keys/ebayKeys.js')
   var request = require('request');
 
   function queryEbay(req,res) {
-    console.log(req.params.query)
-    request('http://open.api.ebay.com/shopping?&version=951&appid=&callName=FindProducts&QueryKeywords=' + req.params.query,
+    request('http://open.api.ebay.com/shopping?&version=951&appid=' + ebayKeys.ebayKeys.appID +'&callName=FindProducts&QueryKeywords=' + req.params.query,
       function(error, response, body) {
       res.json(response)
       })
